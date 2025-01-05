7ab663dff6be08fb6047dd02c2dc1c16531a0589
Houssam Eddine HAMOUICH <itshoussameddine@gmail.com>
2025-01-05 02:31:12 +0100

    enhance game UI and functionality with high score tracking and improved button animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 function App() {
 
     const [start, setStart] = useState(false);
-    const [title, setTitle] = useState('Press Go to start');
+    const [title, setTitle] = useState('Press Go! to Start');
     const [level, setLevel] = useState(0);
+    const [highScore, setHighScore] = useState(0);
     const [gamePattern, setGamePattern] = useState([]);
     const [userPattern, setUserPattern] = useState({list: [], button: ''});
 
     const buttonColors = ['green', 'red', 'yellow', 'blue'];
+    
 
     useEffect(() => {
         if(userPattern.list.length > 0){
@@ -34,31 +36,47 @@ function App() {
             }
             
         }else{
+            if(level > highScore){
+                setHighScore(level);
+                
+            }
             btnError(userPattern.button);
+            playSound('error');
             setGamePattern([]);
             setUserPattern({list: [], button: ''});
             setTitle('Press Go to start');
+            setLevel(0);
             setStart(false);
         }
     }
 
     function animateButton(color){
         const div = document.getElementsByClassName(color)[0];
-        div.classList.add('pressed');
+        div.classList.add('chosenBtn');
         setTimeout(() => {
-            div.classList.remove('pressed')
+            div.classList.remove('chosenBtn')
         }, 300)
     }
+
+    function animateClickedButton(color){
+        const div = document.getElementsByClassName(color)[0];
+        div.classList.add('clicked');
+        setTimeout(() => {
+            div.classList.remove('clicked')
+        }, 150)
+    }
+
     function btnError(e){
         e.target.classList.add('error');
-        document.body.style.backgroundColor = '#E57373';
+        document.body.style.background = '#E57373';
 
         setTimeout(() => {
             e.target.classList.remove('error');
-            document.body.style.backgroundColor = 'white';
+            document.body.style.background = 'linear-gradient(135deg, #f7e8a4, #ffdea0)';;
 
         }, 150)
     }
+
     function startBtn(){
         if(!start){
             setStart(true);
@@ -72,17 +90,22 @@ function App() {
                 list: [...userPattern.list, color],
                 button: e
             });
+            animateClickedButton(color);
+            playSound('click');
         }
+    }
+
+    function playSound(sound){
+        const soundPath = sound === 'click' ? '../public/sounds/click.ogg' : '../public/sounds/error.ogg';
+        const audio = new Audio(soundPath);
+        audio.play();
     }
 
     return (
         <div className='game-container'>
+            <h1 className='high-score'>High Score: {highScore}</h1>
             <h1>{title}</h1>
             <button onClick={startBtn}>Go</button>
-
-            <br></br>
-            <br></br>
-
             <div onClick={(e) => handleGame(e, 'green')} className='green'></div>
             <div onClick={(e) => handleGame(e, 'red')} className='red'></div>
             <div onClick={(e) => handleGame(e, 'yellow')} className='yellow'></div>
